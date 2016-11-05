b7ab188ffb74ed034c280500cf221583ec0d17fd
Rebecca <rebeccal450@hotmail.co.uk>
2016-11-05 20:31:39 +0000

    kinda works

--- pi/25_ultrasonic_ranging.py
+++ pi/25_ultrasonic_ranging.py
@@ -3,47 +3,82 @@
 import RPi.GPIO as GPIO
 import time
 import qrcode
+import pygame
+import Tkinter
+import os
 
 TRIG = 11
 ECHO = 12
+piId = "1"
+
 
 def setup():
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(TRIG, GPIO.OUT)
-	GPIO.setup(ECHO, GPIO.IN)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(TRIG, GPIO.OUT)
+        GPIO.setup(ECHO, GPIO.IN)
 
 def distance():
-	GPIO.output(TRIG, 0)
-	time.sleep(0.000002)
+        GPIO.output(TRIG, 0)
+        time.sleep(0.000002)
 
-	GPIO.output(TRIG, 1)
-	time.sleep(0.00001)
-	GPIO.output(TRIG, 0)
+        GPIO.output(TRIG, 1)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, 0)
 
-	
-	while GPIO.input(ECHO) == 0:
-		a = 0
-	time1 = time.time()
-	while GPIO.input(ECHO) == 1:
-		a = 1
-	time2 = time.time()
+        
+        while GPIO.input(ECHO) == 0:
+                a = 0
+        time1 = time.time()
+        while GPIO.input(ECHO) == 1:
+                a = 1
+        time2 = time.time()
 
-	during = time2 - time1
-	return during * 340 / 2 * 100
+        during = time2 - time1
+        return during * 340 / 2 * 100
 
 def loop():
-	while True:
-		dis = distance()
-		if dis <= 300.00:
-                        img = qrcode.make(#url generated)                               
-		time.sleep(0.3)
+        while True:
+                dis = distance()
+                if dis <= 10.00:
+                        qr = qrcode.QRCode(
+                            version=1,
+                            error_correction=qrcode.constants.ERROR_CORRECT_L,
+                            box_size=10,
+                            border=4,
+                        )
+                        
+                        qr.add_data("www.google.co.uk")# + '/' + str(time.time()))
+                        qr.make(fit=True)
+
+                        img = qr.make_image()
+                        with open(piId + '.png', 'wb') as f:
+                            img.save(f)
+                        f.close()
+
+                        while dis <= 10.00:
+                                
+
+                                pygame.init()
+                                picture=pygame.image.load( piId + '.png')
+                                pygame.display.set_mode(picture.get_size())
+                                main_surface = pygame.display.get_surface()
+                                main_surface.blit(picture, (0,0))
+                                pygame.display.update()
+                                time.sleep(1)
+                                dis  = distance()
+                                
+                        pygame.quit()
+                        os.remove(piId + '.png')
+                
+                                
+                time.sleep(1)
 
 def destroy():
-	GPIO.cleanup()
+        GPIO.cleanup()
 
 if __name__ == "__main__":
-	setup()
-	try:
-		loop()
-	except KeyboardInterrupt:
-		destroy()
+        setup()
+        try:
+                loop()
+        except KeyboardInterrupt:
+                destroy()
--- pi/QRcodeGenerator.py
+++ pi/QRcodeGenerator.py
@@ -1,7 +1,10 @@
+import pygame
 import qrcode
+import Tkinter
+from PIL import Image
+import time
 
-
-piId = "testPi"
+piId = "1"
 
 
 qr = qrcode.QRCode(
@@ -10,7 +13,7 @@ qr = qrcode.QRCode(
     box_size=10,
     border=4,
 )
-qr.add_data('Some data')
+qr.add_data("www.$$$.com")
 qr.make(fit=True)
 
 img = qr.make_image()
@@ -21,3 +24,11 @@ with open(piId + '.png', 'wb') as f:
 
 f.close()
 
+pygame.init()
+picture=pygame.image.load("1.png")
+pygame.display.set_mode(picture.get_size())
+main_surface = pygame.display.get_surface()
+main_surface.blit(picture, (0,0))
+pygame.display.update()
+time.sleep(5)
+pygame.quit()
