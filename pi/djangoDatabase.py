c0b5e67cbbaec6af114dd712def203a6bd0b45b1
Kerry Johnstone <misskerryjohnstone@gmail.com>
2016-11-05 20:20:49 +0000

    Updates to databse in progress, please help, it's slowly killing me #BestCodeEver

--- pi/djangoDatabase.py
+++ pi/djangoDatabase.py
@@ -1,14 +1,39 @@
 from django.db import models
 
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     password = models.CharField(max_length20)
+    achivements = models.#unsure what to add here
 
 CREATE TABLE app_user (
     "id" serial NOT NULL PRIMARY KEY,
     "first_name" varchar(30) NOT NULL,
     "last_name" varchar(30) NOT NULL,
     "password" varchar(20) NOT NULL,
+    "achievements" multiset #unsure of exact implementation
+);
+
+class Routes(models.Model):
+    id = models.AutoField(primary_key=True)
+    posts = models #unsure how to do
+    estimate_time = models.IntegerField()
+    keywords = models #unsure
+
+CREATE TABLE app_routes (
+    "id" serial NOT NULL PRIMARY KEY,
+    "posts" #unsure
+    "estimate_time" integer() NOT NULL,
+    "keywords" multiset #dont know exact implementation
+);
+class Posts(models.Model):
+    name = models.CharField(max_length=20)
+    points = models.IntegerField()
+    description = models.CharField(max_length=250)
+    requirement = models.CharField(max_length=140)
+    post_validated = 
     
     
+    
+
