7d9e7dc4a30f771e54635dbb3bd5a73bab650f87
Kerry Johnstone <misskerryjohnstone@gmail.com>
2016-11-05 23:42:43 +0000

    UPdate to flask, now has route for each oage, need to pass variables to routes

--- pi/main.py
+++ pi/main.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def hello():
     return render_template("home.html", name="Kerry")
 
+@app.route("/route/mainCanalLoop")
+def mainCanalLoop():
+    return render_template("route1.html")
+
+@app.route("/route/lagoon")
+def lagoon():
+    return render_template("route2.html")
+
+@app.route("/posts/kelpie")
+def kelpie():
+    return render_template("kelpie.html")
+t
 if __name__ == "__main__":
     app.run()
     app.jinja_env.line_statement_prefix = '%'
