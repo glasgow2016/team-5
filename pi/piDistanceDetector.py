e52df1ef758412dff2200600b37f9a5e35848e0f
Iain Cattermole <iain_c@live.co.uk>
2016-11-06 02:06:05 +0000

    delete unused file

--- pi/piDistanceDetector.py
+++
@@ -1,9 +0,0 @@
-#distance = distance detected by detector
-
-#while true:
-    #if distance <= 300.00:
-        #generate QR code
-        #keep qr code on screen for 120 secs
-import qrcode
-
-img = qrcode.make('www.google.co.uk')
