10ca7beb46c7d04c89041d73adf7888c8e99ab42
RHollywood <ra_angel@icloud.com>
2016-11-06 00:03:18 +0000

    Update Rewards.py
    #GuyFawkes

--- Website/Rewards.py
+++ Website/Rewards.py
@@ -1,11 +1,12 @@
 import string
 import random
 
+##Check if user has all stickers, then generate reward code
 def ValidateReward(stickerList,n):
     if len(stickerList)==n:
+        
+        ##Generate Random Reward code
         chars=string.ascii_uppercase + string.digits
         print ''.join(random.choice(chars)for _ in range(6))
     
-                      
-
-ValidateReward(stickerList,n)
+##ValidateReward(stickerList,n)
