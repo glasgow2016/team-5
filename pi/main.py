40464b1e13a4febd8d0d84217d38d801def5b7b9
Kerry Johnstone <misskerryjohnstone@gmail.com>
2016-11-06 03:44:52 +0000

    Updated main.py with new routes, began implementing names as variables, but this is still in progress

--- pi/main.py
+++ pi/main.py
@@ -7,22 +7,33 @@ app = Flask(__name__)
 #renders the home page of the webapp
 @app.route("/")
 def hello():
-    return render_template("home.html", name="Jane Smith")
+    return render_template("home.html")
 
 #renders webpage detaling the Main Canal Loop Route
 @app.route("/route/mainCanalLoop")
 def mainCanalLoop():
-    return render_template("route1.html")
+    return render_template("route1.html", name="Jane Smith")
 
 #renders webpage detailing the Lagoon Route
 @app.route("/route/lagoon")
 def lagoon():
-    return render_template("route2.html")
+    return render_template("route2.html", name="Jane Smith")
 
 #renders page delivered to user when they check in at kelpie signpost
 @app.route("/posts/kelpie")
 def kelpie():
-    return render_template("kelpie.html")
+    return render_template("kelpie.html", name="Jane Smith")
+
+#renders page delivered to user when they check in at visitor centre
+@app.route("/posts/visitorCentre")
+def visitorCentre():
+    return render_template("visitor.html", name="Jane Smith")
+
+#renders page delivered to user when they check in at visitor centre
+@app.route("/posts/Lagoon")
+def Lagoon():
+    return render_template("lagoon.html", name="Jane Smith")
+
 
 #runs the web app
 if __name__ == "__main__":
